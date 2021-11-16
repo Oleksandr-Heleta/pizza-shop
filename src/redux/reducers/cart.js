c1aaa95e271cf587c604fd42639f2d0b94c3f572
Oleksandr-Heleta <sanock.geleta@gmail.com>
2021-11-16 14:34:02 +0200

    do responsiv

--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -3,6 +3,12 @@ const initialState = {
     totalPrice: 0,
     totalCount: 0 
 };
+const getSumAndCount = (items) =>{
+    const countArr = Object.values(items).flat();
+    const totalSum = countArr.reduce((sum, obj) => (obj.totalPrice + sum), 0);
+    const totalCount = countArr.reduce((sum, obj) => (obj.totalCount + sum), 0);
+    return [totalSum, totalCount]
+};
 
 const cart = (state = initialState, action) => {
     switch(action.type){
@@ -13,7 +19,7 @@ const cart = (state = initialState, action) => {
                 totalPrice: action.payload.price,
                 totalCount: 1,
             }
-            const secondItem = () =>{
+            const nextItem = () =>{
                 let flag = false;
                 state.items[action.payload.id].map(obj => {
                     if (obj.type === action.payload.type && obj.size === action.payload.size ) { 
@@ -33,18 +39,17 @@ const cart = (state = initialState, action) => {
                 ...state.items,
                 [action.payload.id]: !state.items[action.payload.id]
                  ?  [ firstItem]
-                 : secondItem()
+                 : nextItem()
             }
-            const countArr = Object.values(newItems).flat();
-            const totalSum = countArr.reduce((sum, obj) => (obj.totalPrice + sum), 0);
-            const totalCount = countArr.reduce((sum, obj) => (obj.totalCount + sum), 0);
+            const sumAndCount = getSumAndCount(newItems)
          return {
             ...state,
             items: newItems,
-            totalCount: totalCount,
-            totalPrice: totalSum  
+            totalCount: sumAndCount[1],
+            totalPrice: sumAndCount[0]  
          }    
         };
+
         case 'CLEAR_CART': return {
             ...state,
             items: {},
@@ -52,31 +57,50 @@ const cart = (state = initialState, action) => {
             totalCount: 0 
             
         };
+
         case 'REMOVE_CART_ITEM':  {
-            const newItems = state.items[action.payload.id].filter(obj => 
+            const filterItems = state.items[action.payload.id].filter(obj => 
                 obj.type !== action.payload.type && obj.size !== action.payload.size 
             );
-            const allPizzas = {
+            const newItems = {
                 ...state.items,
-                [action.payload.id]: newItems
+                [action.payload.id]: filterItems
             };
-            const countArr = Object.values(allPizzas).flat();
-            const totalSum = countArr.reduce((sum, obj) => (obj.totalPrice + sum), 0);
-            const totalCount = countArr.reduce((sum, obj) => (obj.totalCount + sum), 0);
-            
+            const sumAndCount = getSumAndCount(newItems)
             return {
-            ...state,
-            items:{
+               ...state,
+               items: newItems,
+               totalCount: sumAndCount[1],
+               totalPrice: sumAndCount[0]  
+            };   
+           };
+
+        case 'REMOVE_PIZZA_CART': { 
+            let ind;
+            const remoweInItems = state.items[action.payload.id].map((obj, index) => {
+                if (obj.type === action.payload.type && obj.size === action.payload.size ) { 
+                    ind = index;
+                    obj.totalCount -= 1;
+                    obj.totalPrice -= obj.price;
+                    return  obj;
+                   }  
+                return obj; 
+            });
+            
+            if (remoweInItems[ind].totalCount < 1)  {remoweInItems.splice(ind, 1)} ;
+            const newItems = {
                 ...state.items,
-                [action.payload.id]: newItems
-            },
-            totalCount: totalCount,
-            totalPrice: totalSum
-        }};
-        case 'REMOVE_PIZZA_CART':  return {
-            ...state,
-            totalCount: action.payload,
+                [action.payload.id]: remoweInItems
+            };
+            const sumAndCount = getSumAndCount(newItems)
+            return {
+               ...state,
+               items: newItems,
+               totalCount: sumAndCount[1],
+               totalPrice: sumAndCount[0]  
+            };
         };
+
         default: return state;
     }
 
